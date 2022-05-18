05e3db9e7de155602f574f54e1739db4eb29d747
primetime71 <shantanu3543@gmail.com>
2022-05-19 00:02:30 +0530

    improved image filter functionality - gallery.html

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,19 +1,40 @@
-const topBtn=document.querySelector(".to-top");
+const topBtn = document.querySelector(".to-top");
 
-window.addEventListener("scroll",()=>{
-    if(window.pageYOffset>100){
-        topBtn.classList.add("active");
-    }
-    else {
-        topBtn.classList.remove("active");
-    }
-})
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    topBtn.classList.add("active");
+  } else {
+    topBtn.classList.remove("active");
+  }
+});
 
-
-function setItem1()
-{
- let kitchen= document.querySelectorAll(".item-1");
- for (let i = 0; i < kitchen.length; i++) {
-    kitchen[i].style.display = "none";
+function resetStyle() {
+  let imgArr = document.querySelectorAll(".item-1,.item-2,.item-3");
+  for (let i = 0; i < imgArr.length; i++) {
+    imgArr[i].style.display = "block";
+  }
 }
+
+function showItem1() {
+  resetStyle();
+  let imgArr = document.querySelectorAll(".item-2,.item-3");
+  for (let i = 0; i < imgArr.length; i++) {
+    imgArr[i].style.display = "none";
+  }
+}
+
+function showItem2() {
+  resetStyle();
+  let imgArr = document.querySelectorAll(".item-1,.item-3");
+  for (let i = 0; i < imgArr.length; i++) {
+    imgArr[i].style.display = "none";
+  }
+}
+
+function showItem3() {
+  resetStyle();
+  let imgArr = document.querySelectorAll(".item-1,.item-2");
+  for (let i = 0; i < imgArr.length; i++) {
+    imgArr[i].style.display = "none";
+  }
 }
